4dc88e022d68205b8f87833a45493ee0cebecad5
Sampath107b <kadimichandu1234@gmail.com>
2025-08-31 16:13:04 +0530

    3rd commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
+import AdminDash from './pages/AdminDash';
+import LoginPage from './pages/LoginPage';
+import PostPage from './pages/PostPage';
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
     <BrowserRouter>
     <div className='app'>
+      <Navbar/>
+
+      <main className=''>     
       <Routes>
-        <Route path='/' element={<h1>Home Page</h1>}  />
-        <Route path='/post/:id' element={<h1>single post page</h1>} />
-        <Route path='/admin/login' element={<h1>Admin login page</h1>} />
+        <Route path='/' element={<HomePage/>}  />
+        <Route path='/post/:id' element={<PostPage/>} />
+        <Route path='/admin/login' element={<LoginPage/>} />
+        <Route path='/admin/dashboard' element={<AdminDash/>}/>
       </Routes>
+      </main> 
 
     </div>
     
